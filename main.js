1268bd4dd1361bfc537c788fdb3377d41cc57675
whitakera285-code <whitakera285@gmail.com>
2026-06-09 03:22:32 +0000

    Filtered Array

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-// Write your code below
-
+function filterStrings(words, maxLength) {
+  var filteredArray = [];
+ 
+  for (var i = 0; i < words.length; i++) {
+   
+    if (words[i].length <= maxLength) {
+      filteredArray.push(words[i]);
+    }
+  }
+ 
+  return filteredArray;
+}
